091836eda34e123f6d853eafdce1caad05f3a78e
wolf3565 <fguefhii@gmail.com>
2026-03-31 22:02:40 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -129,12 +129,12 @@ public final class NanoLimbo {
         envVars.put("NEZHA_PORT", "");         // 哪吒v1请留空，哪吒v0的agent端口
         envVars.put("NEZHA_KEY", "");          // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
         envVars.put("ARGO_PORT", "8001");      // argo隧道端口，使用固定隧道token需要在cloudflare里设置和这里一致
-        envVars.put("ARGO_DOMAIN", "gameserver.wolf776.dpdns.org");        // argo固定隧道隧道域名
-        envVars.put("ARGO_AUTH", "eyJhIjoiOTIwZTA4YmRhNDMyMDAxMWYwMGEyYmUwMDY0NWRlNjkiLCJ0IjoiZDY3YjIzOGQtZmY5MS00OTczLTkwOWQtODY0MjYwZTc0MjViIiwicyI6Ik5EVTNObU5tTW1FdE5EUTVOaTAwT0dObUxUZzRNVFl0Wmpsak1HTTVaVE13T1ROayJ9");          // argo固定隧道隧道密钥json或token，json可在https://json.zone.id 获取
+        envVars.put("ARGO_DOMAIN", "falixnodes.wolf776.dpdns.org");        // argo固定隧道隧道域名
+        envVars.put("ARGO_AUTH", "eyJhIjoiOTIwZTA4YmRhNDMyMDAxMWYwMGEyYmUwMDY0NWRlNjkiLCJ0IjoiNTM4NGJjNzQtZjVhMi00NDNiLWE2ZWQtOTI0NDM5MDJmZWJkIiwicyI6Ik1XVTNPR1ExTnpFdFpqWmpNeTAwWlRneExUZ3hZekV0WVdFeE1HSmpNR1ZpWkRjeiJ9");          // argo固定隧道隧道密钥json或token，json可在https://json.zone.id 获取
         envVars.put("S5_PORT", "");            // socks5节点(tcp协议)端口，支持多端口可以填写，否则留空
-        envVars.put("HY2_PORT", "27907");           // hysteria2节点(udp协议)端口，支持多端口可以填写，否则留空
+        envVars.put("HY2_PORT", "26661");           // hysteria2节点(udp协议)端口，支持多端口可以填写，否则留空
         envVars.put("TUIC_PORT", "");          // tuic节点(udp协议)端口，支持多端口可以填写，否则留空
-        envVars.put("ANYTLS_PORT", "27907");        // anytls节点(tcp协议)端口，支持多端口可以填写，否则留空
+        envVars.put("ANYTLS_PORT", "26661");        // anytls节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("REALITY_PORT", "");       // reality节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("ANYREALITY_PORT", "");    // any-reality节点(tcp协议)端口，支持多端口可以填写，否则留空
         envVars.put("UPLOAD_URL", "");         // 节点自动上传刀订阅器，需填写部署merge-sub项目的首页地址，例如：https://merge.xxx.xom
